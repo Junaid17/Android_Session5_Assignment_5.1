a4c57b6b7c2089f009a435ca1141251cc2deea42
Junaid <junaidmangalwad.26@gmail.com>
2017-08-19 12:56:52 +0530

    first commit

---
+++ app/src/main/java/com/example/jmush/android_session5_assignment_51/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.jmush.android_session5_assignment_51;
+
+import android.animation.Animator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements Animation.AnimationListener{
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView= (TextView) findViewById(R.id.textView);
+        textView.setVisibility(View.VISIBLE);
+        Animation animFadeIn= AnimationUtils.loadAnimation(this,R.anim.fade_in);
+        textView.startAnimation(animFadeIn);
+    }
+
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+}
